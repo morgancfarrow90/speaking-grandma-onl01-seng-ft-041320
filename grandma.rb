f5769905725fd7d3f50d8ffc9333825e36f3db52
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-15 11:51:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,13 +2,13 @@
 
 def speak_to_grandma(phrase) 
   if ${phrase} != speak_to_grandma.upcase
-  puts "HUH! SPEAK UP, SONNY!"
+    return "HUH! SPEAK UP, SONNY!"
 elsif ${phrase} == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
 else ${phrase} == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
-
 
   
   
