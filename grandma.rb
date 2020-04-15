a8dc5c9014780dcf27de3e2c5878476a5a8f0ddc
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-15 11:47:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 
-speak_to_grandma = "Hi, Grandma"
+speak_to_grandma = ""
 
 if speak_to_grandma != speak_to_grandma.upcase
   puts "HUH! SPEAK UP, SONNY!"
@@ -10,7 +10,7 @@ else speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
-  
+
   
   
   
