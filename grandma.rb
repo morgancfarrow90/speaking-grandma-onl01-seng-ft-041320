a0b0b44a9c8ecd94a3329af62fac3827ac99d2f7
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-15 12:11:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == phrase.upcase
-    return "HUH?!, SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
